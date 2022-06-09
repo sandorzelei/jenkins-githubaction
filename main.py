fd78be882f5b30a56f8b93474e443f3cd3b3839c
Sándor Zelei <sandor.zelei@intland.com>
2022-06-09 20:50:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -31,132 +31,11 @@ def main():
 
     g = Github(os.environ.get("INPUT_ACCESS_TOKEN"))
     
-    if username and api_token:
-        auth = (username, api_token)
-    else:
-        auth = None
-        logging.info('Username or token not provided. Connecting without authentication.')
-
-    if parameters:
-        try:
-            parameters = json.loads(parameters)
-        except json.JSONDecodeError as e:
-            raise Exception('`parameters` is not valid JSON.') from e
-    else:
-        parameters = {}
-
-    if cookies:
-        try:
-            cookies = json.loads(cookies)
-        except json.JSONDecodeError as e:
-            raise Exception('`cookies` is not valid JSON.') from e
-    else:
-        cookies = {}
-
-    jenkins = Jenkins(url, auth=auth, cookies=cookies)
-
-    try:
-        jenkins.version
-    except Exception as e:
-        raise Exception('Could not connect to Jenkins.') from e
-
-    logging.info('Successfully connected to Jenkins.')
-
-    queue_item = jenkins.build_job(job_name, **parameters)
-
-    logging.info('Requested to build job.')
-
-    t0 = time()
-    sleep(interval)
-    while time() - t0 < start_timeout:
-        build = queue_item.get_build()
-        if build:
-            break
-        logging.info(f'Build not started yet. Waiting {interval} seconds.')
-        sleep(interval)
-    else:
-        raise Exception(f"Could not obtain build and timed out. Waited for {start_timeout} seconds.")
-
-    build_url = build.url
-    if access_token:
-        issue_comment(f'{display_job_name} - Build started [here]({build_url})')
-
-    logging.info(f"Build URL: {build_url}")
-    print(f"::set-output name=build_url::{build_url}")
-    print(f"::notice title=build_url::{build_url}")
-
-    result=wait_for_build(build,timeout,interval)
-
-    if not access_token:
-        logging.info("No comment.")
-        if result in ('FAILURE', 'ABORTED'):
-            raise Exception(result)
-        return
-
-    body = f'### [{display_job_name} - Build]({build_url}) status returned **{result}**.'
-    t0=time()
-    while time() - t0 < job_query_timeout:
-        try:
-            duration=build.api_json()["duration"]
-            if duration != 0:
-                body+='\n{display_job_name} - Build ran _{build_time} ms_'.format(display_job_name=display_job_name, build_time=duration)
-                break
-        except e:
-            logging.info(f"Build duration unknown:\n{e}")
-        sleep(job_query_interval)
-    else:
-        logging.info("Error fetching build details")
-        body+="\nError fetching build details"
-        issue_comment(body)
-        raise Exception("Error fetching build details")
-
-    test_reports=build.get_test_report()
-    if build.get_test_report() is None:
-        body+="\n_No test were ran_"
-    else:
-        test_reports_json=test_reports.api_json()
-        body+="\n\n## Test Results:\n**Passed: {p}**\n**Failed: {f}**\n**Skipped: {s}**".format(
-        p=test_reports_json["passCount"],
-        f=test_reports_json["failCount"],
-        s=test_reports_json["skipCount"]
-    )
-
-    try:
-         joke = requests.get('https://api.chucknorris.io/jokes/random', timeout=1).json()["value"]
-         body+=f"\n\n>{joke}"
-    except e:
-        logging.info(f"API cannot be called:\n{e}")
-
-    issue_comment(body)
-
-    if result in ('FAILURE', 'ABORTED'):
-        raise Exception(result)
+    getCommitMessages(g)
 
 
-def wait_for_build(build,timeout,interval):
-    build_url=build.url
-    t0 = time()
-    sleep(interval)
-    while time() - t0 < timeout:
-        result = build.result
-        if result == 'SUCCESS':
-            logging.info(f'Build successful')
-            return result
-        if result == 'UNSTABLE':
-            logging.info(f'Build unstable')
-            return result
-        if result in ('FAILURE', 'ABORTED'):
-            logging.info(f'Build status returned "{result}". Build has failed ☹️.')
-            return result
-        logging.info(f'Build not finished yet. Waiting {interval} seconds. {build_url}')
-        sleep(interval)
-    logging.info(f"Build has not finished and timed out. Waited for {timeout} seconds.")
-    return "TIMEOUT"
-
-
-def issue_comment(body):
-    g = Github(os.environ.get("INPUT_ACCESS_TOKEN"))
-
+def getCommitMessages(githubApi):
+    
     github_event_file = open(os.environ.get("GITHUB_EVENT_PATH"), "r")
     github_event = json.loads(github_event_file.read())
     github_event_file.close
@@ -164,15 +43,12 @@ def issue_comment(body):
     pr_repo_name = github_event["pull_request"]["base"]["repo"]["full_name"]
     pr_number = github_event["number"]
 
-    g.get_repo(pr_repo_name).get_pull(pr_number).create_issue_comment(body)
-
-        
-def pr(g):
-    repo = g.get_repo("intland/pr-integration-test")
-    pulls = repo.get_pulls(state='open', sort='created', base='master')
+    commits = githubApi.get_repo(pr_repo_name).get_pull(pr_number).get_commits(body)
     
-    for pr in pulls:
-       print(pr)
+    cnt = 0
+    for c in commits:
+        cnt += 1
+        print(str(cnt) + " :" + c.commit.message)
 
 if __name__ == "__main__":
     main()
